28ff71d242fe73aae63f0380c9bc4a67bc77a395
Gleb <maikezseller@gmail.com>
2022-02-24 02:04:04 +0300

    get items from todo_list

--- src/db.rs
+++ src/db.rs
@@ -1,11 +1,11 @@
-use crate::models::{TodoList};
+use crate::models::{TodoList, TodoItem};
 use deadpool_postgres::Client;
 use tokio_pg_mapper::FromTokioPostgresRow;
 use std::io;
 
 pub async fn get_todos(client: &Client) -> Result<Vec<TodoList>, io::Error> {
 
-    let statement = client.prepare("SELECT * FROM todo_list").await.unwrap();
+    let statement = client.prepare("SELECT * FROM todo_list ORDER BY id DESC").await.unwrap();
     let todos = client.query(&statement, &[])
         .await
         .expect("Error getting todo list")
@@ -14,4 +14,18 @@ pub async fn get_todos(client: &Client) -> Result<Vec<TodoList>, io::Error> {
         .collect::<Vec<TodoList>>();
 
     Ok(todos)
+}
+
+pub async fn get_items(client: &Client, list_id: i32) -> Result<Vec<TodoItem>, io::Error> {
+
+    let statement = client.prepare("SELECT * FROM todo_item WHERE list_id = $1 ORDER BY id").await.unwrap();
+    let items = client.query(&statement, &[&list_id])
+        .await
+        .expect("Error getting todo list")
+        .iter()
+        .map(|row| TodoItem::from_row_ref(row).unwrap())
+        .collect::<Vec<TodoItem>>();
+
+    Ok(items)
+
 }
